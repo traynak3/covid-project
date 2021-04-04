45810ad6ef2e8d47a3a471cb30dc9d498300e6da
Charles Carter <ccc31807@yahoo.com>
2021-04-04 13:54:53 -0400

    first commit of data, dict, and sql

---
+++ sql/ssms-create-census-tables.sql
@@ -0,0 +1,49 @@
+use covid;
+
+bulk insert dbo.husa
+  from 'C:\Users\ccc31\covid-data\psam_husa.csv'
+  with (
+    datafiletype = 'char',
+	fieldterminator = ',',
+	rowterminator = '0x0a',
+	firstrow = 2,
+	format = 'csv'
+	);
+bulk insert dbo.husa
+  from 'C:\Users\ccc31\covid-data\psam_husb.csv'
+  with (
+    datafiletype = 'char',
+	fieldterminator = ',',
+	rowterminator = '0x0a',
+	firstrow = 2,
+	format = 'csv'
+	);
+--(788908 rows affected)
+
+--(759280 rows affected)
+
+--(1658808 rows affected)
+
+--(1580745 rows affected)
+
+-- total rows = 3,239,553
+
+bulk insert dbo.pusa
+  from 'C:\Users\ccc31\covid-data\psam_pusa.csv'
+  with (
+    datafiletype = 'char',
+	fieldterminator = ',',
+	rowterminator = '0x0a',
+	firstrow = 2,
+	format = 'csv'
+	);
+bulk insert dbo.pusa
+  from 'C:\Users\ccc31\covid-data\psam_pusb.csv'
+  with (
+    datafiletype = 'char',
+	fieldterminator = ',',
+	rowterminator = '0x0a',
+	firstrow = 2,
+	format = 'csv'
+	);
+
